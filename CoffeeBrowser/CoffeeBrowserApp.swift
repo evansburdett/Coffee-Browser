447526d2fc8d209a77273e7b53ef2f62b81bb09d
Evan <evansburdett@gmail.com>
2025-07-20 14:39:08 -0500

    Initial Commit

---
+++ CoffeeBrowser/CoffeeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBrowserApp.swift
+//  CoffeeBrowser
+//
+//  Created by Evan Burdett on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
